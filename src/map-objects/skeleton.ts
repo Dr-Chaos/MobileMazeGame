dff7f4c4839ab94fc93f592af1cdca2ab0da3fd2
Nanborg <nicolas.sanchezroca123@gmail.com>
2023-10-20 19:23:23 +0200

    update skeleton.ts degat fire

--- src/map-objects/skeleton.ts
+++ src/map-objects/skeleton.ts
@@ -3,13 +3,14 @@ import { camera } from '../camera';
 import app from '../pixi/initialize';
 import { isColliding } from '../math/collisions';
 import { playerHitbox } from '../player/player';
-
+import { getCoordinates } from '../utils/utils';
 import { atlasLoader } from '../pixi/atlas-loader';
+import { fireball } from '../player/fireball';
 
 type Skeleton = AnimatedSprite & { life: number; damage: number };
-const skeletons: Skeleton[] = [];
+const skeletons: Record<string, Skeleton> = {};
 
-export function createSkeleton(x: number, y: number) {
+export function createSkeleton(x: number, y: number, name: string) {
   const skeleton: Skeleton = new AnimatedSprite(atlasLoader.skeleton.animations.idle) as Skeleton;
   skeleton.scale.set(2);
   skeleton.animationSpeed = 0.17;
@@ -17,17 +18,46 @@ export function createSkeleton(x: number, y: number) {
   skeleton.x = x;
   skeleton.y = y;
   camera.addChild(skeleton);
-  skeleton.life = 5;
-  skeleton.damage = 1;
-  skeletons.push(skeleton);
+  skeleton.life = 50;
+  skeleton.damage = 1000;
+  skeletons[name] = skeleton;
 }
 
 app.ticker.add(() => {
-  for (const skeleton of skeletons) {
-    if (!isColliding(skeleton, playerHitbox)) continue;
-    skeleton.life -= 1;
-    if (skeleton.life > 0) continue;
-    camera.removeChild(skeleton);
+  for (const name in skeletons) {
+    if (name in skeletons) {
+      const skeleton = skeletons[name];
+      const playerX = playerHitbox.x;
+      const playerY = playerHitbox.y;
+
+      const directionX = playerX - skeleton.x;
+      const directionY = playerY - skeleton.y;
+
+      const distance = Math.hypot(directionX, directionY);
+
+      const normalizedDirectionX = directionX / distance;
+      const normalizedDirectionY = directionY / distance;
+
+      const speed = 1;
+      skeleton.x += normalizedDirectionX * speed;
+      skeleton.y += normalizedDirectionY * speed;
+
+      if (isColliding(getCoordinates(skeleton), playerHitbox)) {}
+    }
+  }
+});
+
+app.ticker.add(() => {
+  for (const name in skeletons) {
+    if (name in skeletons) {
+      const skeleton = skeletons[name];
+      if (isColliding(getCoordinates(skeleton), fireball)) { // remplacer playerhitbox par la fireball, car c'est la fireball qui fait des degats pas la hitbox du player
+        skeleton.life -= 1;
+        if (skeleton.life <= 0) {
+          camera.removeChild(skeleton);
+        }
+      }
+    }
   }
 });
 
