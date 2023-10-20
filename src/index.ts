520a84cbd8c3c545b1686390fea93042082132fe
Nanborg <nicolas.sanchezroca123@gmail.com>
2023-10-20 21:23:09 +0200

    BOSS ON FIRE

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
-// import './utils/atlas-generator';
+// import './atlas-generator';
 import './map/map-draw-layers';
 import './map/map-collisions';
 // import './map-objects/traps';
-// import './player/fireball';
+import './player/fireball';
 import app from './pixi/initialize';
 import { camera } from './camera';
 // import { playerHitbox } from './player/player';
 import './player/move'; // handle move inputs
-import { createSpike } from './map-objects/spike';
 import { createSkeleton } from './map-objects/skeleton';
-import levier, { createLevier } from './map-objects/levier';
+import './map-objects/skeletonpath';
+import { inputMovementDirection } from './input/mouvement';
+import { boss, createBoss } from './map-objects/boss';
 
 app.stage.addChild(camera); // create the world / camera
-// createSpike(40, -40);
-// createSkeleton(0, 0);
-createLevier(0, 0);
 
+// créer un skeleton ici
+createSkeleton(0, 0);
+createBoss(1, 1);
 // Press D key to display debug logs
 document.addEventListener('keydown', (event) => {
   if (event.code !== 'Digit1') return;
