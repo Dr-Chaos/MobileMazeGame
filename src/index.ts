68a34b83a8352c89ba39e9e3efc51cf36aa10085
Dr-Chaos <crossazriel@gmail.com>
2023-10-22 03:51:41 +0200

    Fix index ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 // import './utils/atlas-generator';
-import './map/map-draw-layers';
+import './map/map-layers';
 import './map/map-collisions';
 import app from './pixi/initialize';
 import { camera } from './camera';
 import './player/move'; // handle move inputs
 import { atlasLoader } from './pixi/atlas-loader';
+import './player/fireball';
 
 app.stage.addChild(camera); // create the world / camera
 
