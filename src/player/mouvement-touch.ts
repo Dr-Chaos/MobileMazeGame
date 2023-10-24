2870faf178c06288c5cee2667d5b2ab0ca0e7171
Dr-Chaos <crossazriel@gmail.com>
2023-10-24 09:43:38 +0200

    Improve touch

--- src/player/mouvement-touch.ts
+++ src/player/mouvement-touch.ts
@@ -5,28 +5,41 @@ let previousTouch = {
   y: 0,
 };
 
+// fix relative andabsolute positions
+let originTouch = {
+  x: 0,
+  y: 0,
+};
+
+document.addEventListener('touchstart', (event) => {
+  originTouch = {
+    x: event.touches[0].clientX,
+    y: event.touches[0].clientY,
+  };
+});
+
 document.addEventListener('touchmove', (event) => {
   const userTouch = {
     x: event.changedTouches[0].clientX,
     y: event.changedTouches[0].clientY,
   };
 
-  console.log(event.changedTouches);
+  // console.log(event.changedTouches);
 
   // console.log(userTouch);
 
-  if (userTouch.x + 100 < window.innerWidth / 2) {
+  if (userTouch.x + 100 < originTouch.x) {
     console.log('Left');
     directionHistory.x.push(-1);
-  } else if (userTouch.x - 100 > window.innerWidth / 2) {
+  } else if (userTouch.x - 100 > originTouch.x) {
     console.log('right');
     directionHistory.x.push(1);
   }
 
-  if (userTouch.y + 100 < window.innerHeight / 2) {
+  if (userTouch.y + 100 < originTouch.y) {
     console.log('top');
     directionHistory.y.push(-1);
-  } else if (userTouch.y - 100 > window.innerHeight / 2) {
+  } else if (userTouch.y - 100 > originTouch.y) {
     console.log('bottom');
     directionHistory.y.push(1);
   }
