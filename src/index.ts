6f90849cfd81a1799798c64ca4dc8688db4d5e05
Nanborg <nicolas.sanchezroca123@gmail.com>
2023-10-25 21:58:44 +0200

    temporary+index

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,19 @@ import './player/move'; // handle move inputs
 import './player/fireball';
 import './map-objects/skull';
 import './map-objects/boss/boss';
+import {
+  SkeletonStates, createSkeleton, gameLoop, skeletons,
+} from './temporary';
 // aaa
 
 app.stage.addChild(camera); // create the world / camera
+
+// ici on va créer un squelette
+createSkeleton();
+app.ticker.add(gameLoop);
+
+document.addEventListener('keydown', (event) => {
+  if (event.code === 'Digit1') skeletons[0].state = SkeletonStates.Idle;
+  if (event.code === 'Digit2') skeletons[0].state = SkeletonStates.Walk;
+  if (event.code === 'Digit3') skeletons[0].state = SkeletonStates.Death;
+});
